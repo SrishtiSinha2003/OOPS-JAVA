2012b0fab682f4220d9d51e71f6f521a164ae22f
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-01-14 16:44:55 +0530

    Update Number.java

---
+++ DAY-1/Number.java
@@ -0,0 +1,10 @@
+public class Number {
+  public static void main(String args[]) {
+    int number=10,number2=110;
+        System.out.println("value of number is "+number+" and number2 is "+number2);
+        int n1=2;
+        int n2=3;
+        float sum=n1+n2;
+        System.out.println("values of  sum is "+n1+ "and "+n2+" is "+sum);
+  }
+}
