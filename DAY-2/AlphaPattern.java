8f1cf8927d4717fd5e2ad553bf201dfc11df673b
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-01-21 15:26:58 +0530

    Added new files

---
+++ DAY-2/AlphaPattern.java
@@ -0,0 +1,11 @@
+public class AlphaPattern {
+        public static void main(String[] args) {
+            int n = 5; 
+            for (int i = n; i >= 1; i--) {
+                for (char j = 'A'; j < 'A' + i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
