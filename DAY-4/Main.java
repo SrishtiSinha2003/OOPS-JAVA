3e79a7cf8e2cc131f61f680fe2b3f9a116fa4b9d
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-02-04 15:01:01 +0530

    Added new Files

---
+++ DAY-4/Main.java
@@ -0,0 +1,22 @@
+class CarClass {
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public CarClass(String make, String model, int year, String color){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+    public void displayDetails() {
+        System.out.println("Make: " + make + ", Model: " + model + ", Year: " + year + ", Color: " + color);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        CarClass myCar = new CarClass("Toyota", "Corolla", 2020, "Blue");
+        myCar.displayDetails();
+    }
+}
---
+++ DAY-4/Product.java
@@ -0,0 +1,13 @@
+class Product{
+    String productName;
+    int productId;
+    double productPrice;
+    int quantityInStock;
+
+    public Product(String productName, int productID, double productPrice, int quantityInStock){
+        this.productName = productName;
+        this.productId = productID;
+        this.productPrice = productPrice;
+        this.quantityInStock = quantityInStock;
+    }
+}
