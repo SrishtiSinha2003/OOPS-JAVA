f188b6e0eca6b2a66029cb059e7ee188fe5c7c43
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-02-04 18:32:23 +0530

    Updated Files

---
+++ DAY-4/Object.java
@@ -0,0 +1,19 @@
+class ObjectCounter {
+    static int count = 0; 
+
+    ObjectCounter() {
+        count++; 
+    }
+
+    static void displayCount() {
+        System.out.println("Number of objects created: " + count);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        ObjectCounter obj1 = new ObjectCounter();
+        ObjectCounter obj2 = new ObjectCounter();
+        ObjectCounter obj3 = new ObjectCounter();
+        ObjectCounter.displayCount();
+    }
+}
--- DAY-4/Product.java
+++ DAY-4/Product.java
@@ -1,16 +1,37 @@
-class Product{
+import java.util.ArrayList;
+
+class Product {
     String productName;
     int productId;
     double productPrice;
     int quantityInStock;
 
-    public Product(String productName, int productID, double productPrice, int quantityInStock){
+    public Product(String productName, int productID, double productPrice, int quantityInStock) {
         this.productName = productName;
         this.productId = productID;
         this.productPrice = productPrice;
         this.quantityInStock = quantityInStock;
     }
+
     public void discount(double discountPercent) {
         productPrice -= productPrice * (discountPercent / 100);
     }
+    public void displayProductDetails() {
+        System.out.println("Product Name: " + productName + ", Product ID: " + productId + ", Price: " + productPrice + ", Quantity in Stock: " + quantityInStock);
+    }
+    public static void main(String[] args) {
+        ArrayList<Product> productList = new ArrayList<>();
+        productList.add(new Product("Laptop", 101, 50000, 10));
+        productList.add(new Product("Smartphone", 102, 20000, 50));
+        productList.add(new Product("Headphones", 103, 1500, 100));
+
+        for (Product product : productList) {
+            product.discount(10); 
+        }
+        for (Product product : productList) {
+            product.displayProductDetails();
+        }
+    }
 }
+
+
